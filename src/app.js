5ad79ca89f6e2c7b2c999aa7757748ee0998a1a1
jayampathib <wmjb.weerasinghe@gmail.com>
2020-03-26 19:45:18 +0800

    3.3.2 Create Book Model in model folder

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 const mongoose = require('mongoose');
 
-var svrApp= express();
+var svrApp = express();
 
 //Connect to Mongo DB
-const db = mongoose.connect('mongodb://localhost/bookAPI',{ useNewUrlParser: true })
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.log("Connection FAil\n"+err));
+const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log("Connection FAil\n" + err));
 
 
 var port = process.env.PORT || 3000;
@@ -15,18 +15,18 @@ const bookRouter = express.Router();
 
 // bookRouter object is connfigured to read '/books' url and return 
 bookRouter.route('/books')
-    .get((req,res)=>{
-        const response = {hello :'This is my first API Get call'};
+    .get((req, res) => {
+        const response = { hello: 'This is my first API Get call' };
         res.json(response);
     });
 //use above configured route object
-svrApp.use('/api',bookRouter);
+svrApp.use('/api', bookRouter);
 
-svrApp.get('/',(req,res)=> {
+svrApp.get('/', (req, res) => {
     res.send('Welcome to REST Api...');
 });
 
-svrApp.listen(port,()=>{
-    console.log('Running On Port :' +port);
+svrApp.listen(port, () => {
+    console.log('Running On Port :' + port);
 })
 
