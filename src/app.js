aec243aa15a6ee670e1dce4f6b6f29647e312f00
jayampathib <wmjb.weerasinghe@gmail.com>
2020-03-26 17:53:59 +0800

    3.1 Create Route object and configure get

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var svrApp= express();
+
 var port = process.env.PORT || 3000;
+//Create router object useing express module
+const bookRouter = express.Router();
+
+// bookRouter object is connfigured to read '/books' url and return 
+bookRouter.route('/books')
+    .get((req,res)=>{
+        const response = {hello :'This is my first API Get call'};
+        res.json(response);
+    });
+//use above configured route object
+svrApp.use('/api',bookRouter);
 
 svrApp.get('/',(req,res)=> {
     res.send('Welcome to REST Api...');
