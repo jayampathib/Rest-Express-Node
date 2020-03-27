c5263bfe18d244a84860583444f61178340ab58f
jayampathib <wmjb.weerasinghe@gmail.com>
2020-03-27 18:48:28 +0800

    6.3 setup test code

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -4,7 +4,11 @@ function booksController(Book) {
         const book = new Book(req.body);
         book.save();
         console.log(book);
-        return res.status(201).json(book);
+
+        // execute in 2 lines. beacause test will not be run with this way
+        // test has seperate implementation for each method
+        res.status(201);
+        return res.json(book);
     }
     return { post }
 }
