ab4be29ff31329d972e9bac7358eb8b1e327f44d
jayampathib <wmjb.weerasinghe@gmail.com>
2020-03-27 19:45:50 +0800

    6.5 add Validation and complete ACT of testcase

--- UnitTest/TestController/booksControllerTest.js
+++ UnitTest/TestController/booksControllerTest.js
@@ -3,36 +3,35 @@ const should = require('should');
 const sinon = require('sinon');
 const bookController = require('../../src/controllers/booksController');
 
-describe('Book Control test', () => {
-    describe('Post', () => {
-        it('should not allow an  empty title on post', () => {
+describe('Book Controller Tests:', () => {
+  describe('Post', () => {
+    it('should not allow an empty title on post', () => {
 
-            // -----------START : SET UP MOCK OBJECTS [ARRANGE] -----------------
+         // -----------START : SET UP MOCK OBJECTS [ARRANGE] -----------------
             // Save function is implmented inside mongoose. but here we don't need to check mongoose library save function
             // we create a dummy "Book" object and dummy function for save, and assume save happen prpoerly
-            const Book = function (book) { this.save = () => { } };
+      const Book = function (book) { this.save = () => {}};
 
-            // setup mock Request object
-            const req={
-                body:{
-                    author :'Jon'
-                }
-            };
+      const req = {
+        body: {
+          author: 'Jon'
+        }
+      };
 
-            const res={
-                status : sinon.spy(),
-                send :sinon.spy(),
-                json :sinon.spy()
-            };
-            // -----------END : SET UP MOCK OBJECTS  [ARRANGE] ----------------
+      const res={
+        status : sinon.spy(),
+        send :sinon.spy(),
+        json :sinon.spy()
+      };
+      // -----------END : SET UP MOCK OBJECTS  [ARRANGE] ----------------
 
             // -----------ACT ----------------------------------------------
-            const controller =bookController(Book); // since javascript dont have type for objects, we can easily create "Book" object
-            controller.post(req,res);
-
+      const controller =bookController(Book); // since javascript dont have type for objects, we can easily create "Book" object
+      controller.post(req,res);
+      
             // -----------ASSERT ----------------------------------------------
-            res.status.calledWith(400).should.equal(true,`Bad Status ${res.status.args[0][0]}`);
-            Response.send.calledWith('Title is required').should.equal(true);
-        });
+      res.status.calledWith(400).should.equal(true,`Bad Status ${res.status.args[0][0]}`);
+      res.send.calledWith('Title is required').should.equal(true);
     });
+  });
 });
--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,16 +1,20 @@
 function booksController(Book) {
     function post(req, res) {
-        console.log(req.body);
-        const book = new Book(req.body);
-        book.save();
-        console.log(book);
+      console.log(req.body);
+      const book = new Book(req.body);
+      if (!req.body.title) {
+        res.status(400);
+        return res.send('Title is required');
+      }
+      book.save();
+      console.log(book);
 
-        // execute in 2 lines. beacause test will not be run with this way
-        // test has seperate implementation for each method
-        res.status(201);
-        return res.json(book);
+      // execute in 2 lines. beacause test will not be run with this way
+      // test has seperate implementation for each method
+      res.status(201);
+      return res.json(book);
     }
-    return { post }
-}
-
-module.exports = booksController;
+    return { post };
+  }
+  
+  module.exports = booksController;
